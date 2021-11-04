64a12e963e5a670ae83617880da0efc35415be25
rjmorla <41564261+rjmorla@users.noreply.github.com>
2021-11-04 02:51:45 -0700

    added post related activities and layouts removed built in login activity

---
+++ app/src/main/java/com/example/interestchat/ViewMyPosts.java
@@ -0,0 +1,14 @@
+package com.example.interestchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewMyPosts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_my_posts);
+    }
+}
