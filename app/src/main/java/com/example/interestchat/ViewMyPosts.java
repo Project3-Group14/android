c08249dd329af430e9a7bd75e8ed74dd1daef8b6
rjmorla <41564261+rjmorla@users.noreply.github.com>
2021-12-09 14:45:12 -0800

    added more functionality to addPost need figure out how to get userId from props

--- app/src/main/java/com/example/interestchat/ViewMyPosts.java
+++ app/src/main/java/com/example/interestchat/ViewMyPosts.java
@@ -3,10 +3,17 @@ package com.example.interestchat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class  ViewMyPosts extends AppCompatActivity {
     ArrayList<Post> posts;
@@ -18,14 +25,39 @@ public class  ViewMyPosts extends AppCompatActivity {
         // Lookup the recyclerview in activity layout
         RecyclerView rvContacts = (RecyclerView) findViewById(R.id.viewMyPostsRecyclerView);
 
-        // Initialize contacts
-        posts = Post.createPostList(5);
-        // Create adapter passing in the sample user data
-        PostAdapter adapter = new PostAdapter(posts);
-        // Attach the adapter to the recyclerview to populate items
-        rvContacts.setAdapter(adapter);
-        // Set layout manager to position the items
-        rvContacts.setLayoutManager(new LinearLayoutManager(this));
-        // That's all!
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://group14-chat.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        InterestChatApi interestChatApi = retrofit.create(InterestChatApi.class);
+
+        Intent intent = getIntent();
+        String userId = intent.getStringExtra("loginUserId");
+        Call<List<Post>> call = interestChatApi.getPostsByUserId(userId);
+
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    return;
+                }
+                List<Post> posts = response.body();
+                // Initialize posts
+                // Create adapter passing in the sample user data
+                PostAdapter adapter = new PostAdapter(posts);
+                // Attach the adapter to the recyclerview to populate items
+                rvContacts.setAdapter(adapter);
+                // Set layout manager to position the items
+                rvContacts.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                // That's all!
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+
+            }
+        });
+
     }
 }
